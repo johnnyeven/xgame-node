fc7217e3559cd2c63948008107e4610522e76ebd
johnnyeven_work <johnnyeven@gmail.com>
2014-10-28 18:22:36 +0800

    1

--- routes/game.js
+++ routes/game.js
@@ -17,6 +17,26 @@ function checkLogin(req, res, next) {
 function checkRole(req, res, next) {
 	if(!req.session.role) {
 		return res.redirect('/role');
+	} else {
+		var mongo_connect = require('../modules/MongoConnection');
+		var Role = require('../modules/Role');
+		mongo_connect(function(db) {
+			Role.findOne({
+				account_id: req.session.user.id
+			}, function(err, role) {
+				db.close();
+				if(err) {
+					err.status = 500;
+					return next(err, req, res);
+				}
+				if(role) {
+					req.session.role = role;
+				} else {
+					req.session.role = null;
+					return res.redirect('/role');
+				}
+				next();
+			});
+		});
 	}
-	next();
 }
